f21eaef3a2062f209956dd16509a7a0b51bd8681
Franco Dominguez <138259949+Fdominguez1998@users.noreply.github.com>
2024-10-10 22:20:32 -0300

    Update Punto_B.sh

--- RTA_SCRIPT_Examen_20241009/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241009/Punto_B.sh
@@ -67,15 +67,15 @@ echo
 
 echo "monto en forma permanente"
 
-echo "/dev/sdc1 /Examenes-UTN/Alumno1/parcial1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc2 /Examenes-UTN/Alumno1/parcial2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc3 /Examenes-UTN/Alumno1/parcial3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc5 /Examenes-UTN/Alumno2/parcial1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc6 /Examenes-UTN/Alumno2/parcial2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc7 /Examenes-UTN/Alumno2/parcial3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc8 /Examenes-UTN/Alumno3/parcial1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc9 /Examenes-UTN/Alumno3/parcial2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc10 /Examenes-UTN/Alumno3/parcial3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc1 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc2 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc3 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc5 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc6 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc7 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc8 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc9 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc10 /Examenes-UTN/alumno-3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
 echo "/dev/sdc11 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
 
 mount -a
