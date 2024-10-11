95a7f827a7e703669d96cf24d389dd02aee05981
Franco Dominguez <138259949+Fdominguez1998@users.noreply.github.com>
2024-10-10 22:24:31 -0300

    Update Punto_C.sh

--- RTA_SCRIPT_Examen_20241009/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241009/Punto_C.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 
 echo "obtengo la contraseña de mi usuario vagrant y la guardo en una variable"
@@ -6,15 +6,15 @@ echo "obtengo la contraseña de mi usuario vagrant y la guardo en una variable"
 CLAVE_HASH=$(sudo grep "vagrant:" /etc/shadow | awk -F: '{print $2}')
 
 echo "Primero creo los grupos"
-groupadd p1c2_2024_gAlumno
-groupadd p1c2_2024_gProfesores
+sudo groupadd p1c2_2024_gAlumno
+sudo groupadd p1c2_2024_gProfesores
 
 echo "Creo los usuarios, le asigno su grupo y creo su contraseña"
 
-useradd -m -s /bin/bash -c "Pepito" -G p1c2_2024_gAlumno -p "CLAVE_HASH" p1c2_2024_A1
-useradd -m -s /bin/bash -c "Maria" -G p1c2_2024_gAlumno -p "CLAVE_HASH" p1c2_2024_A2
-useradd -m -s /bin/bash -c "Batman" -G p1c2_2024_gAlumno -p "CLAVE_HASH" p1c2_2024_A3
-useradd -m -s /bin/bash -c "Gonzalo" -G p1c2_2024_gProfesores -p "CLAVE_HASH" p1c2_2024_P1
+sudo useradd -m -s /bin/bash -c "Pepito" -G p1c2_2024_gAlumno -p "CLAVE_HASH" p1c2_2024_A1
+sudo useradd -m -s /bin/bash -c "Maria" -G p1c2_2024_gAlumno -p "CLAVE_HASH" p1c2_2024_A2
+sudo useradd -m -s /bin/bash -c "Batman" -G p1c2_2024_gAlumno -p "CLAVE_HASH" p1c2_2024_A3
+sudo useradd -m -s /bin/bash -c "Gonzalo" -G p1c2_2024_gProfesores -p "CLAVE_HASH" p1c2_2024_P1
 
 
 echo "Modifico los permisos de los usuarios"
