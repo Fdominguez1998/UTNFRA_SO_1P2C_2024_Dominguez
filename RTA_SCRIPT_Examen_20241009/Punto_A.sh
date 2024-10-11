706c2793ecf82e31eee835a50e18bea7e317c315
Franco Dominguez <138259949+Fdominguez1998@users.noreply.github.com>
2024-10-10 22:13:22 -0300

    Update Punto_A.sh

--- RTA_SCRIPT_Examen_20241009/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241009/Punto_A.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Creo la estructura"
-sudo mkdir -p /Examenes-UTN/{Alumno{1..3}/parcial{1..3},profesores}
+sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial_{1..3},profesores}
 
 echo
-echo"Muestro"
+echo "Muestro"
 sudo tree /Examenes-UTN
